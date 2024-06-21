7cf64290a44d606ea28e32f3c7536018b8764346
dimitri-code-it <dimitri@code-it.fr>
2024-06-21 11:02:56 +0200

    1.0.0.3

--- projects/ngx-code-it/src/lib/ngx-code-it.service.ts
+++ projects/ngx-code-it/src/lib/ngx-code-it.service.ts
@@ -8,18 +8,18 @@ export class NgxCodeItService {
 
   constructor() { }
   LogUpdater(params: any) {
-    // ...
+    console.log('LogUpdater', params);
   }
   
   LogEffect(params: any) {
-    // ...
+    console.log('LogEffect', params);
   }
   
   LogObservable(groupMessage: string, observable: Observable<any>, initialState: any) {
-   
+    console.log('LogObservable', groupMessage, observable, initialState);
   }
   
   LogState(params: any) {
-    // ...
+    console.log('LogState', params);
   }
 }
